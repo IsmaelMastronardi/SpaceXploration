27040907fe5e2d8f08ad2e7fb975db911fd99980
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-28 16:42:24 -0500

    Adding Basic rocket structure
    Adding Basic rocket structure

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import rocketsReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
+    rockets: rocketsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
