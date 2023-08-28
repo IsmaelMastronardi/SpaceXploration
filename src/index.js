01aad2c9f28eff35ec13c6cbbcb1687f8488e727
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-28 17:23:54 -0300

    add provider

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
