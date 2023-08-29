219746fac319affa57b1bc9c904303fb751d83f1
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-29 18:01:25 -0300

    add styles for badges

--- src/components/Missions/singleMissions.js
+++ src/components/Missions/singleMissions.js
@@ -1,23 +1,37 @@
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Badge, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { memberStatus } from '../../redux/Missions/missionsSlice';
 
 const SingleMission = ({ obj }) => {
   const dispatch = useDispatch();
   let membership = '';
+  let joinButton = '';
+  let buttonVariant = '';
+  let missionBadge = '';
   if (obj.reserved === undefined || obj.reserved === false) {
     membership = <span className="notMemberSpan">NOT A MEMBER</span>;
+    joinButton = 'Join Mission !';
+    buttonVariant = 'outline-secondary';
+    missionBadge = 'secondary';
   } else {
     membership = <span className="isMemberSpan">Active Member</span>;
+    joinButton = 'Leave Mission';
+    buttonVariant = 'outline-danger';
+    missionBadge = 'info';
   }
   return (
     <tr>
       <td>{obj.mission_name}</td>
       <td>{obj.description}</td>
-      <td>{membership}</td>
-      <td>
-        <Button variant="outline-secondary" onClick={() => dispatch(memberStatus(obj.mission_id))}>Join Mission</Button>
+      <td className="align-middle"><Badge bg={missionBadge} className="whiteColor">{membership}</Badge></td>
+      <td className="align-middle">
+        <Button
+          variant={buttonVariant}
+          onClick={() => dispatch(memberStatus(obj.mission_id))}
+        >
+          {joinButton}
+        </Button>
       </td>
     </tr>
   );
