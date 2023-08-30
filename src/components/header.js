7f1f76d5416bc72a8f9fa4f8cfa832cac667fc2a
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-30 00:01:49 -0300

    fixed linters issue

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/order */
 import { Link, Route, Routes } from 'react-router-dom';
+import {
+  Container, Nav, Navbar,
+} from 'react-bootstrap';
 import Rockets from './Rockets/rockets';
 import Missions from './Missions/missions';
-import {
-  Col, Container, Nav, Navbar, Row,
-} from 'react-bootstrap';
 import planet from '../assets/icons/planet.png';
 
-// const Header = () => (
-//   <>
-//     <header>
-//       <ul>
-//         <li>
-//           <Link to="/">Rockets</Link>
-//         </li>
-//         <li>
-//           <Link to="/Missions">Missions</Link>
-//         </li>
-//       </ul>
-//     </header>
-//     <Routes>
-//       <Route path="/" element={<Rockets />} />
-//       <Route path="/Missions" element={<Missions />} />
-//     </Routes>
-//   </>
-// );
 const Header = () => (
   <>
     <Navbar className="justify-content-between  align-items-center border-bottom navPadding" sticky="top" bg="white">
