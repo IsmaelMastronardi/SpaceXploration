a15096d3ecb8eede79cf0d41fe28e967d2b7b2e0
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-30 19:26:54 -0300

    made merge with My Profiles changes from other branch

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -16,25 +16,18 @@ const MyProfile = () => {
         <h2 className="text-center text-sm-start">Reserved Missions</h2>
         <ul className="container-fluid d-flex flex-column justify-content-center ps-0 me-5 list-unstyled">
           {filterMission.map((mission) => (
-            <li className="border border-success p-2 border-opacity-25" key={mission.mission_id}>
-              <h3 className="mb-0 fs-5">{mission.mission_name}</h3>
+            <li key={mission.mission_id} className="myProfileListItem border border-success p-2 border-opacity-25 align-items-center">
+              <h3 className="myProfileMissionTitle mb-0 fs-5"><a href={mission.wikipedia} target="_blank" rel="noreferrer">{mission.mission_name}</a></h3>
+              <Button
+                variant="outline-danger"
+                onClick={() => dispatch(memberStatus(mission.mission_id))}
+                className="btn-max-heigth"
+              >
+                Leave Mission
+              </Button>
             </li>
           ))}
         </ul>
-         <ul className="container-fluid d-flex flex-column justify-content-center ps-0 me-5 list-unstyled">
-           {filterMission.map((mission) => (
-             <li key={mission.mission_id} className="myProfileListItem border border-success p-2 border-opacity-25">
-               <h3 className="myProfileMissionTitle mb-0 fs-5"><a href={mission.wikipedia} target="_blank" rel="noreferrer">{mission.mission_name}</a></h3>
-               <Button
-                 variant="outline-danger"
-                 onClick={() => dispatch(memberStatus(mission.mission_id))}
-                 className="btn-max-heigth"
-               >
-                 Leave Mission
-               </Button>
-             </li>
-           ))}
-         </ul>
       </div>
       <div className="d-flex flex-column w-100 p-3">
         <h2 className="text-center text-sm-start">Reserved Rockets</h2>
